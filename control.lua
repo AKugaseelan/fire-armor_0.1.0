3e3448a34e0a8e4fc2b036ae4f152bb27563146e
Athiraiyan Kugaseelan <athiraiyan.kugaseelan@basevision.ch>
2022-03-29 15:24:44 +0200

    Added control scripting to mod

---
+++ control.lua
@@ -0,0 +1,13 @@
+-- control.lua
+
+script.on_event(defines.events.on_player_changed_position,
+function(event)
+    local player = game.get_player(event.player_inde) -- get the player that moved
+    -- if they're weaing our armor
+    if player.character and player.get_inventory(defines.inventory.character_armor).get_item_count("fire-armor") >= 1
+    then
+        -- create the fire where they're standing
+        player.surface.create_entity{name = "fire-flame", position = player.position, force = "neutral"}
+    end
+end
+)
